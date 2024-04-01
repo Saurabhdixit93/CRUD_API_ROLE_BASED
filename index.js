ce64b9aba0d86f551eb56754b98cde4e330e81e1
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2024-04-02 01:32:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
+//  Middleware to check server status
+const serverStatusMiddleware = (req, res, next) => {
+  if (req.path === "/") {
+    return res.status(200).json({
+      status: "active",
+      message: "Server is active and working perfectly",
+    });
+  } else {
+    next();
+  }
+};
+
+app.use(serverStatusMiddleware);
+
 app.use("/", indexRoutes);
 
 connectDB().then(() => {
